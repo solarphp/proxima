67e98bdc0f1b40441ce78d1adcc04c631697de3f
Paul M. Jones <pmjones@solarphp.com>
2009-11-09 21:45:58 +0000

    add some minimal docs

---
+++ docs/system-config.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This is the suggested config file, to be placed in `$system/config.php`.
+ */
+$config = array();
+$system = dirname(__FILE__);
+$config['Solar']['system'] = $system;
+
+/**
+ * default configs for each vendor
+ */
+include "$system/source/solar/config/default.php";
+include "$system/source/proxima/config/default.php";
+
+/**
+ * project overrides
+ */
+
+// database
+$config['Solar_Sql'] = array(
+    'adapter' => 'Solar_Sql_Adapter_Sqlite',
+    'name'    => "$system/sqlite/proxima.sq3",
+);
+
+// front controller
+$config['Solar_Controller_Front'] = array(
+    'classes' => array(
+        'Proxima_App',
+        'Solar_App',
+    ),
+    'disable'  => array(),
+    'default' => 'bookmarks',
+    'rewrite' => array(),
+    'routing' => array(),
+);
+
+// model catalog
+$config['Solar_Sql_Model_Catalog']['classes'] = array(
+    'Proxima_Model',
+);
+
+// authentication
+$config['Solar_Auth'] = array(
+    'adapter'    => 'Solar_Auth_Adapter_Sql',
+    'table'      => 'members',
+    'handle_col' => 'handle',
+    'passwd_col' => 'passwd',
+    'email_col'  => 'email',
+    'uri_col'    => 'uri',
+    'uid_col'    => 'id',
+);
+
+// access control
+$config['Solar_Access'] = array(
+    'adapter' => 'Solar_Access_Adapter_File',
+    'file'    => "$system/source/proxima/config/access.txt",
+);
+
+/**
+ * done!
+ */
+return $config;
