5d8ff5cad4c306010d1c063a1b733922d28a356c
Paul M. Jones <pmjones@solarphp.com>
2009-11-09 21:45:41 +0000

    add locale translation

--- Proxima/App/Members/Locale/en_US.php
+++ Proxima/App/Members/Locale/en_US.php
@@ -12,5 +12,5 @@ return array(
     'TEXT_FORGOT'               => 'Enter your email address below, then press "Send". We will send you a link to reset your password.',
     'TEXT_PASSWD'               => 'Please enter a new password.',
     'TEXT_SENT_FORGOT_EMAIL'    => 'We have emailed a link to your "{:email}" address so you can reset your password.',
-    'TEXT_WELCOME'              => 'Welcome to your new application.',
+    'ERR_NO_RECORDS'            => 'No member records found.',
 );
